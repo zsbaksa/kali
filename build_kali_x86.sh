30c5cf4ef3f9e00d91b97dbacba3dea7ff53dec7
zsbaksa <zsbaksa@szintezis-net.hu>
2019-01-15 19:50:49 +0100

    build xfce x86

---
+++ build_kali_x86.sh
@@ -0,0 +1,6 @@
+apt-get update -y
+apt install -y curl git live-build cdebootstrap
+git clone git://git.kali.org/live-build-config.git
+cd live-build-config/
+sed -i 's/586-pae/586/g' auto/config
+./build.sh --arch i386 --variant xfce --verbose

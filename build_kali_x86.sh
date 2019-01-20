107355006bceed9677b2d89f7f05c2113240cc28
zsbaksa <zsbaksa@szintezis-net.hu>
2019-01-20 22:50:15 +0100

    kali list chroot extended

--- build_kali_x86.sh
+++ build_kali_x86.sh
@@ -4,4 +4,13 @@ apt install -y curl git live-build cdebootstrap
 git clone git://git.kali.org/live-build-config.git
 cd live-build-config/
 sed -i 's/586-pae/586/g' auto/config
+cat << EOF >> kali-config/variant-xfce/package-lists/kali.list.chroot
+bettercap
+lighttpd
+hostapd
+isc-dhcp-server
+gqrx-sdr
+golang
+isc-dhcp-server
+EOF
 ./build.sh --arch i386 --variant xfce --verbose
